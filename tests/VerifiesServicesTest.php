e9bc3074a365f9dbf76d27dd7079b3683c26029e
Mark Topper <mark@ulties.com>
2017-01-09 20:51:51 +0100

    Update tests
    Also created `it_can_verify_facebook_the_old_way` and `it_can_verify_slack`.

--- tests/VerifiesServicesTest.php
+++ tests/VerifiesServicesTest.php
@@ -28,10 +28,56 @@ class VerifiesServicesTest extends PHPUnit_Framework_TestCase
 
         $verification = new VerifyServices();
         $verification->request = new Request($data);
+        $response = $verification->verifyServices([
+            'facebook' => 'facebook_token'
+        ]);
+
+        $this->assertInstanceOf(Response::class, $response);
+        $this->assertSame('facebook_hub_challenge', $response->getContent());
+    }
+
+    /** @test */
+    public function it_can_verify_facebook_the_old_way() // depricated
+    {
+        $data = [
+            'hub_challenge' => 'facebook_hub_challenge',
+            'hub_mode' => 'subscribe',
+            'hub_verify_token' => 'facebook_token',
+        ];
+        $request = m::mock(Request::class.'[getContent]');
+        $request->shouldReceive('getContent')
+            ->once()
+            ->andReturn(json_encode($data));
+
+        $verification = new VerifyServices();
+        $verification->request = new Request($data);
         $response = $verification->verifyServices('facebook_token');
 
         $this->assertInstanceOf(Response::class, $response);
         $this->assertSame('facebook_hub_challenge', $response->getContent());
+    }
+    
+    /** @test */
+    public function it_can_verify_slack()
+    {
+        $data = [
+            'type' => 'url_verification',
+            'challenge' => 'slack_challenge',
+            'token' => 'slack_token',
+        ];
+        $request = m::mock(Request::class.'[getContent]');
+        $request->shouldReceive('getContent')
+            ->once()
+            ->andReturn(json_encode($data));
+
+        $verification = new VerifyServices();
+        $verification->request = new Request($data);
+        $response = $verification->verifyServices([
+            'slack' => 'slack_token'
+        ]);
+
+        $this->assertInstanceOf(Response::class, $response);
+        $this->assertSame('slack_challenge', $response->getContent());
     }
 }
 
