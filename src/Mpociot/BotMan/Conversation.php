6466a8f25f8cd789fa19221c8e8db44bd9d20895
Christoph Rumpel <c.rumpel@kabsi.at>
2017-01-23 17:16:52 +0100

    Fix styling

--- src/Mpociot/BotMan/Conversation.php
+++ src/Mpociot/BotMan/Conversation.php
@@ -49,7 +49,7 @@ abstract class Conversation
     {
         $conversation = $this->bot->getStoredConversation();
 
-        if (!$question instanceof Question && !$question) {
+        if (! $question instanceof Question && ! $question) {
             $question = unserialize($conversation['question']);
         }
 
