8c8e9de7b7a796b9fccae9f63f1da4b02cca1ed6
Bao Pham <gbaopham@gmail.com>
2017-02-17 23:34:16 +0800

    Allow to reply with a file (Slack RTM only) (#220)

--- src/Mpociot/BotMan/Drivers/SlackRTMDriver.php
+++ src/Mpociot/BotMan/Drivers/SlackRTMDriver.php
@@ -2,6 +2,7 @@
 
 namespace Mpociot\BotMan\Drivers;
 
+use Slack\File;
 use Mpociot\BotMan\User;
 use Slack\RealTimeClient;
 use Mpociot\BotMan\Answer;
@@ -89,7 +90,7 @@ class SlackRTMDriver implements DriverInterface
     }
 
     /**
-     * @param string|Question $message
+     * @param string|Question|IncomingMessage $message
      * @param Message $matchingMessage
      * @param array $additionalParameters
      * @return $this
@@ -101,10 +102,19 @@ class SlackRTMDriver implements DriverInterface
             'as_user' => true,
         ], $additionalParameters);
 
+        $fileToUpload = null;
+
         if ($message instanceof IncomingMessage) {
             $parameters['text'] = $message->getMessage();
             if (! is_null($message->getImage())) {
                 $parameters['attachments'] = json_encode([['title' => $message->getMessage(), 'image_url' => $message->getImage()]]);
+            }
+
+            if (! empty($message->getFilePath()) && file_exists($message->getFilePath())) {
+                $fileToUpload = (new File())
+                    ->setTitle(basename($message->getFilePath()))
+                    ->setPath($message->getFilePath())
+                    ->setInitialComment($message->getMessage());
             }
         } elseif ($message instanceof Question) {
             $parameters['text'] = '';
@@ -113,7 +123,13 @@ class SlackRTMDriver implements DriverInterface
             $parameters['text'] = $message;
         }
 
-        $this->client->apiCall('chat.postMessage', $parameters);
+        if (empty($fileToUpload)) {
+            $this->client->apiCall('chat.postMessage', $parameters);
+        } else {
+            $this->client->fileUpload($fileToUpload, [$matchingMessage->getChannel()]);
+        }
+
+        return $this;
     }
 
     /**
