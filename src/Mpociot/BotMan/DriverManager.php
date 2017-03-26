e8536bca93c206f78f54eba3db0041db43ea333c
ribb3r <ribb3r@users.noreply.github.com>
2017-03-26 20:42:17 +0200

    #260 Added FacebookOptinDriver to support facebook checkbox plugin (#286)
    * #260 Added FacebookOptinDriver to support facebook checkbox plugin
    
    * #260 Apply fixes from StyleCI
    
    * #260 Fixed test

--- src/Mpociot/BotMan/DriverManager.php
+++ src/Mpociot/BotMan/DriverManager.php
@@ -15,6 +15,7 @@ use Mpociot\BotMan\Interfaces\HttpInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Mpociot\BotMan\Drivers\BotFrameworkDriver;
 use Mpociot\BotMan\Interfaces\DriverInterface;
+use Mpociot\BotMan\Drivers\FacebookOptinDriver;
 use Mpociot\BotMan\Drivers\FacebookPostbackDriver;
 
 class DriverManager
@@ -26,6 +27,7 @@ class DriverManager
         SlackDriver::class,
         FacebookDriver::class,
         FacebookPostbackDriver::class,
+        FacebookOptinDriver::class,
         TelegramDriver::class,
         BotFrameworkDriver::class,
         NexmoDriver::class,
