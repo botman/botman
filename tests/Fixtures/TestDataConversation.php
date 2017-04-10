1d146a0c9926883228f0ea98a63415e369a1523a
Marcel Pociot <m.pociot@gmail.com>
2017-04-10 12:16:16 +0000

    Apply fixes from StyleCI

--- tests/Fixtures/TestDataConversation.php
+++ tests/Fixtures/TestDataConversation.php
@@ -15,43 +15,43 @@ class TestDataConversation extends Conversation
     {
         $this->ask('What do you want to test?', function (Answer $answer) {
             if ($answer->getText() === 'images') {
-                $this->askForImages('Please supply an image', function($images) {
+                $this->askForImages('Please supply an image', function ($images) {
                     $this->say($images[0]);
                 });
             } elseif ($answer->getText() === 'custom_image_repeat') {
-                $this->askForImages('Please supply an image', function($images) {
+                $this->askForImages('Please supply an image', function ($images) {
                     $this->say($images[0]);
-                }, function(Answer $answer) {
+                }, function (Answer $answer) {
                     $this->say('That is not an image...');
                 });
             } elseif ($answer->getText() === 'videos') {
-                $this->askForVideos('Please supply a video', function($images) {
+                $this->askForVideos('Please supply a video', function ($images) {
                     $this->say($images[0]);
                 });
             } elseif ($answer->getText() === 'custom_video_repeat') {
-                $this->askForVideos('Please supply a video', function($images) {
+                $this->askForVideos('Please supply a video', function ($images) {
                     $this->say($images[0]);
-                }, function(Answer $answer) {
+                }, function (Answer $answer) {
                     $this->say('That is not a video...');
                 });
             } elseif ($answer->getText() === 'audio') {
-                $this->askForAudio('Please supply an audio', function($images) {
+                $this->askForAudio('Please supply an audio', function ($images) {
                     $this->say($images[0]);
                 });
             } elseif ($answer->getText() === 'custom_audio_repeat') {
-                $this->askForAudio('Please supply an audio', function($images) {
+                $this->askForAudio('Please supply an audio', function ($images) {
                     $this->say($images[0]);
-                }, function(Answer $answer) {
+                }, function (Answer $answer) {
                     $this->say('That is not an audio...');
                 });
             } elseif ($answer->getText() === 'location') {
-                $this->askForLocation('Please supply a location', function(Location $location) {
+                $this->askForLocation('Please supply a location', function (Location $location) {
                     $this->say($location->getLatitude().':'.$location->getLongitude());
                 });
             } elseif ($answer->getText() === 'custom_location_repeat') {
-                $this->askForLocation('Please supply a location', function(Location $location) {
+                $this->askForLocation('Please supply a location', function (Location $location) {
                     $this->say($location->getLatitude().':'.$location->getLongitude());
-                }, function(Answer $answer) {
+                }, function (Answer $answer) {
                     $this->say('That is not a location...');
                 });
             }
