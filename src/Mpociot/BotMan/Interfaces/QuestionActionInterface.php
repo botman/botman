4ec75717455ab36e3ad56677dbdff8b75185d17e
Marcel Pociot <m.pociot@gmail.com>
2017-04-13 14:15:44 +0200

    Started working on Slack menu integration

---
+++ src/Mpociot/BotMan/Interfaces/QuestionActionInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Mpociot\BotMan\Interfaces;
+
+interface QuestionActionInterface
+{
+
+    /**
+     * Array representation of the question action.
+     *
+     * @return array
+     */
+    public function toArray();
+}
