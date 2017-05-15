0efff0334ca29b136191dd01e81fee38b851e1a8
Marcel Pociot <m.pociot@gmail.com>
2017-05-15 11:53:39 +0200

    Use extended user information when retrieving user details

--- src/Drivers/Slack/SlackDriver.php
+++ src/Drivers/Slack/SlackDriver.php
@@ -275,7 +275,15 @@ class SlackDriver extends Driver
      */
     public function getUser(Message $matchingMessage)
     {
-        return new User($matchingMessage->getSender());
+        $response = $this->sendRequest('users.info', [
+            'user' => $matchingMessage->getSender()
+        ], $matchingMessage);
+        try {
+            $content = json_decode($response->getContent());
+            return new User($content->user->id, $content->user->profile->first_name, $content->user->profile->last_name, $content->user->name);
+        } catch (\Exception $e) {
+            return new User($matchingMessage->getSender());
+        }
     }
 
     /**
