abf719ee21c9a103151a80f4f2e117991175afc0
Marcel Pociot <m.pociot@gmail.com>
2017-05-15 10:24:44 +0200

    Added tests, nitpicks

--- src/Facebook/ElementButton.php
+++ src/Facebook/ElementButton.php
@@ -20,7 +20,7 @@ class ElementButton
     protected $payload;
 
     /** @var string */
-    protected $webview_height_ratio = 'full';
+    protected $webview_height_ratio = self::RATIO_FULL;
 
     /** @var string */
     protected $webview_share_button = '';
@@ -32,8 +32,13 @@ class ElementButton
     const TYPE_ACCOUNT_UNLINK = 'account_unlink';
     const TYPE_WEB_URL = 'web_url';
     const TYPE_PAYMENT = 'payment';
+    const TYPE_POSTBACK = 'postback';
     const TYPE_SHARE = 'element_share';
     const TYPE_CALL = 'phone_number';
+
+    const RATIO_COMPACT = 'compact';
+    const RATIO_TALL = 'tall';
+    const RATIO_FULL = 'full';
 
     /**
      * @param string $title
@@ -91,7 +96,7 @@ class ElementButton
      * @param string $fallback_url
      * @return $this
      */
-    public function fallback_url($fallback_url)
+    public function fallbackUrl($fallback_url)
     {
         $this->fallback_url = $fallback_url;
 
@@ -120,11 +125,11 @@ class ElementButton
     }
 
     /**
-     * set ratio to one of "compact", "tall", "full".
+     * Set ratio to one of RATIO_COMPACT, RATIO_TALL, RATIO_FULL.
      * @param string $ratio
      * @return $this
      */
-    public function heightRatio($ratio = 'full')
+    public function heightRatio($ratio = self::RATIO_FULL)
     {
         $this->webview_height_ratio = $ratio;
 
@@ -144,7 +149,7 @@ class ElementButton
             $buttonArray['title'] = $this->title;
         }
 
-        if ($this->type === 'postback') {
+        if ($this->type === self::TYPE_POSTBACK) {
             $buttonArray['payload'] = $this->payload;
         } else {
             $buttonArray['url'] = $this->url;
