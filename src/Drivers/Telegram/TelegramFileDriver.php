2c4734747f61b2153e4350bf004e4a1fcfc6271e
Sergio Brighenti <sergio@brighenti.me>
2017-06-05 13:50:32 +0200

    styleci fixes

--- src/Drivers/Telegram/TelegramFileDriver.php
+++ src/Drivers/Telegram/TelegramFileDriver.php
@@ -48,13 +48,13 @@ class TelegramFileDriver extends TelegramDriver
         $path = json_decode($response->getContent());
 
         // In case of file too large, this return only the attachment with the original payload, not the link.
-	    // This need a proper logging and exception system in the future
-	    $url = null;
+        // This need a proper logging and exception system in the future
+        $url = null;
         if (isset($path->result)) {
-        	$url = 'https://api.telegram.org/file/bot'.$this->config->get('telegram_token').'/'.$path->result->file_path;
+            $url = 'https://api.telegram.org/file/bot'.$this->config->get('telegram_token').'/'.$path->result->file_path;
         }
 
-        return [new File($url, $file),];
+        return [new File($url, $file)];
     }
 
     /**
