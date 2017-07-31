b01614e0f57a795a6422dc468b838f8b84d9f00e
Marcel Pociot <m.pociot@gmail.com>
2017-07-31 11:43:20 +0200

    Add WebAccess interface

---
+++ src/Interfaces/WebAccess.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace BotMan\BotMan\Interfaces;
+
+interface WebAccess {
+	/**
+	 * Get the instance as a web accessible array.
+	 * This will be used within the WebDriver.
+	 *
+	 * @return array
+	 */
+	public function toWebDriver();
+}
