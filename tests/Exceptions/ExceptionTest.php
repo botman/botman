16d8f38b299b7e587ea6df4fad940172061cdb16
Sergio Brighenti <sergio@brighenti.me>
2017-08-01 22:02:07 +0200

    Accept exception handlers without closures (#490)
    * Accept exception handlers without closures
    
    * styleci

--- tests/Exceptions/ExceptionTest.php
+++ tests/Exceptions/ExceptionTest.php
@@ -10,6 +10,7 @@ use BotMan\BotMan\BotManFactory;
 use Illuminate\Support\Collection;
 use BotMan\BotMan\Cache\ArrayCache;
 use BotMan\BotMan\Drivers\Tests\FakeDriver;
+use BotMan\BotMan\Tests\Fixtures\TestClass;
 use BotMan\BotMan\Messages\Incoming\IncomingMessage;
 
 class ExceptionTest extends PHPUnit_Framework_TestCase
@@ -64,5 +65,25 @@ class ExceptionTest extends PHPUnit_Framework_TestCase
         });
 
         $botman->listen();
+    }
+
+    /** @test */
+    public function it_catches_exceptions_without_closures()
+    {
+        $botman = $this->getBot([
+            'sender' => 'UX12345',
+            'recipient' => 'general',
+            'message' => 'Hi Julia',
+        ]);
+
+        $botman->exception(Exception::class, TestClass::class.'@exceptionHandler');
+
+        $botman->hears('Hi Julia', function () {
+            throw new Exception('Whoops');
+        });
+
+        $botman->listen();
+
+        $this->assertTrue(TestClass::$called);
     }
 }
