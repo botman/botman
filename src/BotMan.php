ab4892a8784735d8227669621da2d7a26fb2b6ff
Akash4927 <akashsrivastava4927@gmail.com>
2017-12-07 14:01:05 +0530

    Remove messagesHandled() function check from Botman.php

--- src/BotMan.php
+++ src/BotMan.php
@@ -383,15 +383,6 @@ class BotMan
                 if ($this->loadedConversation === false) {
                     $this->callMatchingMessages();
                 }
-
-                /*
-                 * If the driver has a  "messagesHandled" method, call it.
-                 * This method can be used to trigger driver methods
-                 * once the messages are handles.
-                 */
-                if (method_exists($this->getDriver(), 'messagesHandled')) {
-                    $this->getDriver()->messagesHandled();
-                }
             }
 
             $this->firedDriverEvents = false;
