129e43e7d70c0ef8aaf12c663d81e1c6dbabecab
Pete Lawrence <petejlawrence@icloud.com>
2018-02-19 21:09:06 +0000

    Add the ability to add extras onto Attachments

--- src/Messages/Attachments/Attachment.php
+++ src/Messages/Attachments/Attachment.php
@@ -3,11 +3,15 @@
 namespace BotMan\BotMan\Messages\Attachments;
 
 use BotMan\BotMan\Interfaces\WebAccess;
+use Illuminate\Support\Collection;
 
 abstract class Attachment implements WebAccess
 {
     /** @var mixed */
     protected $payload;
+
+    /** @var array */
+    protected $extras = [];
 
     /**
      * Attachment constructor.
@@ -24,5 +28,30 @@ abstract class Attachment implements WebAccess
     public function getPayload()
     {
         return $this->payload;
+    }
+
+    /**
+     * @param string $key
+     * @param mixed $value
+     * @return Attachment
+     */
+    public function addExtras($key, $value)
+    {
+        $this->extras[$key] = $value;
+
+        return $this;
+    }
+
+    /**
+     * @param string|null $key
+     * @return array
+     */
+    public function getExtras($key = null)
+    {
+        if (! is_null($key)) {
+            return Collection::make($this->extras)->get($key);
+        }
+
+        return $this->extras;
     }
 }
