6518267b931a9563a254c4e5d86b9389c2b88608
Marcel Pociot <m.pociot@gmail.com>
2018-05-02 09:51:53 +0200

    Apply fixes from StyleCI (#798)

--- tests/Messages/MatcherTest.php
+++ tests/Messages/MatcherTest.php
@@ -2,122 +2,121 @@
 
 namespace BotMan\BotMan\tests\Messages;
 
+use PHPUnit\Framework\TestCase;
+use BotMan\BotMan\Messages\Matcher;
 use BotMan\BotMan\Messages\Incoming\Answer;
 use BotMan\BotMan\Messages\Incoming\IncomingMessage;
-use BotMan\BotMan\Messages\Matcher;
-use PHPUnit\Framework\TestCase;
 
 class MatcherTest extends TestCase
 {
-
     /**
      * Dataprovider:
-     *  [[$input, $pattern, $expected]]
+     *  [[$input, $pattern, $expected]].
      *
      * @return array
      */
     public function incoming_message_provider()
     {
         $messages = [
-            ['foo', 'foo', true,],
-            ['foo ', 'foo', true,],
-            ['foo ', 'foo ', true,],
+            ['foo', 'foo', true],
+            ['foo ', 'foo', true],
+            ['foo ', 'foo ', true],
 
-            ['foo', '{command}', true,],
-            ['foo ', '{command}', true,],
-            ['foo ', '{command} ', true,],
+            ['foo', '{command}', true],
+            ['foo ', '{command}', true],
+            ['foo ', '{command} ', true],
 
-            ['call me foo', 'call me {name}', true,],
-            ['call me foo ', 'call me {name}', true,],
-            ['call me foo ', 'call me {name} ', true,],
+            ['call me foo', 'call me {name}', true],
+            ['call me foo ', 'call me {name}', true],
+            ['call me foo ', 'call me {name} ', true],
 
-            ['call me foo baz', 'call me {name} {surname}', true,],
-            ['call me foo baz ', 'call me {name} {surname}', true,],
-            ['call me foo baz ', 'call me {name} {surname} ', true,],
+            ['call me foo baz', 'call me {name} {surname}', true],
+            ['call me foo baz ', 'call me {name} {surname}', true],
+            ['call me foo baz ', 'call me {name} {surname} ', true],
 
-            ['/foo', '/foo', true,],
-            ['/foo ', '/foo', true,],
-            ['/foo ', '/foo ', true,],
+            ['/foo', '/foo', true],
+            ['/foo ', '/foo', true],
+            ['/foo ', '/foo ', true],
 
-            ['/foo', '/{command}', true,],
-            ['/foo ', '/{command}', true,],
-            ['/foo ', '/{command} ', true,],
+            ['/foo', '/{command}', true],
+            ['/foo ', '/{command}', true],
+            ['/foo ', '/{command} ', true],
 
-            ['/call me foo', '/call me {name}', true,],
-            ['/call me foo ', '/call me {name}', true,],
-            ['/call me foo ', '/call me {name} ', true,],
+            ['/call me foo', '/call me {name}', true],
+            ['/call me foo ', '/call me {name}', true],
+            ['/call me foo ', '/call me {name} ', true],
 
-            ['/call me foo baz', '/call me {name} {surname}', true,],
-            ['/call me foo baz ', '/call me {name} {surname}', true,],
-            ['/call me foo baz ', '/call me {name} {surname} ', true,],
+            ['/call me foo baz', '/call me {name} {surname}', true],
+            ['/call me foo baz ', '/call me {name} {surname}', true],
+            ['/call me foo baz ', '/call me {name} {surname} ', true],
 
-            ['!foo', '!foo', true,],
-            ['!foo ', '!foo', true,],
-            ['!foo ', '!foo ', true,],
+            ['!foo', '!foo', true],
+            ['!foo ', '!foo', true],
+            ['!foo ', '!foo ', true],
 
-            ['!foo', '!{command}', true,],
-            ['!foo ', '!{command}', true,],
-            ['!foo ', '!{command} ', true,],
+            ['!foo', '!{command}', true],
+            ['!foo ', '!{command}', true],
+            ['!foo ', '!{command} ', true],
 
-            ['!call me foo', '!call me {name}', true,],
-            ['!call me foo ', '!call me {name}', true,],
-            ['!call me foo ', '!call me {name} ', true,],
+            ['!call me foo', '!call me {name}', true],
+            ['!call me foo ', '!call me {name}', true],
+            ['!call me foo ', '!call me {name} ', true],
 
-            ['!call me foo baz', '!call me {name} {surname}', true,],
-            ['!call me foo baz ', '!call me {name} {surname}', true,],
-            ['!call me foo baz ', '!call me {name} {surname} ', true,],
+            ['!call me foo baz', '!call me {name} {surname}', true],
+            ['!call me foo baz ', '!call me {name} {surname}', true],
+            ['!call me foo baz ', '!call me {name} {surname} ', true],
 
-            ['@foo', '@foo', true,],
-            ['@foo ', '@foo', true,],
-            ['@foo ', '@foo ', true,],
+            ['@foo', '@foo', true],
+            ['@foo ', '@foo', true],
+            ['@foo ', '@foo ', true],
 
-            ['@foo', '@{command}', true,],
-            ['@foo ', '@{command}', true,],
-            ['@foo ', '@{command} ', true,],
+            ['@foo', '@{command}', true],
+            ['@foo ', '@{command}', true],
+            ['@foo ', '@{command} ', true],
 
-            ['@call me foo', '@call me {name}', true,],
-            ['@call me foo ', '@call me {name}', true,],
-            ['@call me foo ', '@call me {name} ', true,],
+            ['@call me foo', '@call me {name}', true],
+            ['@call me foo ', '@call me {name}', true],
+            ['@call me foo ', '@call me {name} ', true],
 
-            ['@call me foo baz', '@call me {name} {surname}', true,],
-            ['@call me foo baz ', '@call me {name} {surname}', true,],
-            ['@call me foo baz ', '@call me {name} {surname} ', true,],
+            ['@call me foo baz', '@call me {name} {surname}', true],
+            ['@call me foo baz ', '@call me {name} {surname}', true],
+            ['@call me foo baz ', '@call me {name} {surname} ', true],
 
-            ['#foo', '#foo', true,],
-            ['#foo ', '#foo', true,],
-            ['#foo ', '#foo ', true,],
+            ['#foo', '#foo', true],
+            ['#foo ', '#foo', true],
+            ['#foo ', '#foo ', true],
 
-            ['#foo', '#{command}', true,],
-            ['#foo ', '#{command}', true,],
-            ['#foo ', '#{command} ', true,],
+            ['#foo', '#{command}', true],
+            ['#foo ', '#{command}', true],
+            ['#foo ', '#{command} ', true],
 
-            ['#call me foo', '#call me {name}', true,],
-            ['#call me foo ', '#call me {name}', true,],
-            ['#call me foo ', '#call me {name} ', true,],
+            ['#call me foo', '#call me {name}', true],
+            ['#call me foo ', '#call me {name}', true],
+            ['#call me foo ', '#call me {name} ', true],
 
-            ['#call me foo baz', '#call me {name} {surname}', true,],
-            ['#call me foo baz ', '#call me {name} {surname}', true,],
-            ['#call me foo baz ', '#call me {name} {surname} ', true,],
+            ['#call me foo baz', '#call me {name} {surname}', true],
+            ['#call me foo baz ', '#call me {name} {surname}', true],
+            ['#call me foo baz ', '#call me {name} {surname} ', true],
 
-            ['!@#2f00', '!@#2f00', true,],
-            ['!@#2f00 ', '!@#2f00', true,],
-            ['!@#2f00 ', '!@#2f00 ', true,],
+            ['!@#2f00', '!@#2f00', true],
+            ['!@#2f00 ', '!@#2f00', true],
+            ['!@#2f00 ', '!@#2f00 ', true],
 
-            ['!@#2c@ll m3 f00', '!@#2c@ll m3 {nam3}', true,],
-            ['!@#2c@ll m3 f00 ', '!@#2c@ll m3 {nam3}', true,],
-            ['!@#2c@ll m3 f00 ', '!@#2c@ll m3 {nam3} ', true,],
+            ['!@#2c@ll m3 f00', '!@#2c@ll m3 {nam3}', true],
+            ['!@#2c@ll m3 f00 ', '!@#2c@ll m3 {nam3}', true],
+            ['!@#2c@ll m3 f00 ', '!@#2c@ll m3 {nam3} ', true],
 
-            ['!@#2c@ll m3 f00 baz', '!@#2c@ll m3 {nam3} {surnam3}', true,],
-            ['!@#2c@ll m3 f00 baz ', '!@#2c@ll m3 {nam3} {surnam3}', true,],
-            ['!@#2c@ll m3 f00 baz ', '!@#2c@ll m3 {nam3} {surnam3} ', true,],
+            ['!@#2c@ll m3 f00 baz', '!@#2c@ll m3 {nam3} {surnam3}', true],
+            ['!@#2c@ll m3 f00 baz ', '!@#2c@ll m3 {nam3} {surnam3}', true],
+            ['!@#2c@ll m3 f00 baz ', '!@#2c@ll m3 {nam3} {surnam3} ', true],
 
-            [' foo', 'foo', false,],
-            [' foo', 'foo ', false,],
-            [' foo ', 'foo ', false,],
+            [' foo', 'foo', false],
+            [' foo', 'foo ', false],
+            [' foo ', 'foo ', false],
 
-            ['foo', 'baz', false,],
-            ['foo ', 'baz', false,],
-            ['foo ', 'baz ', false,],
+            ['foo', 'baz', false],
+            ['foo ', 'baz', false],
+            ['foo ', 'baz ', false],
         ];
 
         return $messages;
