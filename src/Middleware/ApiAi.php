dc5e71adc81d9c80247c437dbb2bd0b7a4fadf74
gary houbre <ghoubre@sqli.com>
2019-06-11 10:48:47 +0200

    Update UnitTest

--- src/Middleware/ApiAi.php
+++ src/Middleware/ApiAi.php
@@ -110,7 +110,7 @@ class ApiAi implements MiddlewareInterface
         $response = $this->getResponse($message);
 
         $reply = isset($response->result->fulfillment->speech) ? $response->result->fulfillment->speech : '';
-        $messages = isset($response->result->fulfillment->messages) ? $response->result->fulfillment->messages : '';
+        $messages = isset($response->result->fulfillment->messages) ? $response->result->fulfillment->messages : [];
         $action = isset($response->result->action) ? $response->result->action : '';
         $actionIncomplete = isset($response->result->actionIncomplete) ? (bool) $response->result->actionIncomplete : false;
         $intent = isset($response->result->metadata->intentName) ? $response->result->metadata->intentName : '';
