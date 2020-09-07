8333e1b3008bae2838e151daf839c61d465adcfe
Zaher Ghaibeh <zaher@zah.me>
2020-09-07 07:13:46 +0300

    wip

--- tests/Storages/RedisStorageTest.php
+++ tests/Storages/RedisStorageTest.php
@@ -108,7 +108,6 @@ class RedisStorageTest extends TestCase
      */
     protected function getRedisHost()
     {
-        echo getenv('REDIS_HOST');
         return getenv('REDIS_HOST') ?? '127.0.0.1';
     }
 
@@ -119,7 +118,6 @@ class RedisStorageTest extends TestCase
      */
     protected function getAuthRedisPort()
     {
-        echo getenv('REDIS_PORT');
         return (int) (getenv('REDIS_PORT') ?? 6380);
     }
 }
