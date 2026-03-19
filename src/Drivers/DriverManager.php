7fd09ba75c4c3912e1722c344bbd6126186d188e
Kirill <nitogel@gmail.com>
2026-03-19 12:08:42 +0200

    FIX PHP 8.4 deprecated implicitly nullable parameters (#1428)
    * Update DriverManager.php
    
    PHP 8.4 deprecated implicitly nullable parameters
    
    * Fixes the depreciation issues
    Use nullable types in method signatures

--- src/Drivers/DriverManager.php
+++ src/Drivers/DriverManager.php
@@ -59,7 +59,7 @@ class DriverManager
      * @param Request|null $request
      * @return mixed|HttpDriver|NullDriver
      */
-    public static function loadFromName($name, array $config, Request $request = null)
+    public static function loadFromName($name, array $config, ?Request $request = null)
     {
         /*
         * Use the driver class basename without "Driver" if we're dealing with a
@@ -149,7 +149,7 @@ class DriverManager
      * @param Request|null $request
      * @return bool
      */
-    public static function verifyServices(array $config, Request $request = null)
+    public static function verifyServices(array $config, ?Request $request = null)
     {
         $request = (isset($request)) ? $request : Request::createFromGlobals();
         foreach (self::getAvailableHttpDrivers() as $driver) {
